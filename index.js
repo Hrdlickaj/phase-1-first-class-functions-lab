50dd828cf1165e59cb3806b95cf4392e31c571c5
Jenna Hrdlicka <hrdlickajenna@gmail.com>
2022-04-16 19:19:20 -0600

    commit tree dcfd343abc330756cfe48043af0136aabc017b1f

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    drivers = ['driver1', 'driver2', 'driver3', 'driver4'];
+    return driversList = slice(0,1);
+}
+
